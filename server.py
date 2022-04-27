bdc8743506182ff42652619880595ae70f88acf4
debughunt <huntercollinray@gmail.com>
2022-04-27 11:21:52 -0400

    View counter

---
+++ server.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, request, redirect, session
+app = Flask(__name__)
+app.secret_key = "jsdfagliiasdufhaoeoh"
+
+
+@app.route("/")
+def index():
+    if "visits" in session:
+        session["visits"]+= 1
+    else:
+        session["visits"] = 1
+    return render_template("index.html", views = session["visits"])
+
+
+@app.route("/twoViews", methods=["POST"])
+def addTwo():
+    session["visits"] += 1
+    return redirect("/")
+
+
+
+
+@app.route("/destroy_session", methods=["POST"])
+def clearVis():
+    session.clear()
+    return redirect("/")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)
